b50188d27a5b5af1ba774f982bfa650b0f13edca
myeomgeun <118319841+myeomgeun@users.noreply.github.com>
2024-05-21 17:26:15 +0900

    마켓3pc 퍼블리싱

--- assets/js/design.js
+++ assets/js/design.js
@@ -88,29 +88,29 @@ $(document).ready(function () {
 //제품슬라이드
 $(document).ready(function () {
   $('.sliderWrap').slick({
-    slide: 'div', //슬라이드 되어야 할 태그
-    infinite: true, //무한 반복 옵션     
-    slidesToShow: 3, // 한 화면에 보여질 컨텐츠 개수
-    slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
-    speed: 500, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-    arrows: true, // 옆으로 이동하는 화살표 표시 여부
-    dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
-    autoplay: false, // 자동 스크롤 사용 여부
-    autoplaySpeed: 2000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-    pauseOnHover: true, // 슬라이드 이동    시 마우스 호버하면 슬라이더 멈추게 설정
-    vertical: false, // 세로 방향 슬라이드 옵션
+    slide: 'div', 
+    infinite: true,
+    slidesToShow: 3, 
+    slidesToScroll: 1, 
+    speed: 500, 
+    arrows: true, 
+    dots: false, 
+    autoplay: false, 
+    autoplaySpeed: 2000, 
+    pauseOnHover: true, 
+    vertical: false, 
     prevArrow: "<button type='button' class='slickPrev'>Previous</button>",
     nextArrow: "<button type='button' class='slickNext'>Next</button>",
-    draggable: true, //드래그 가능 여부 
-    responsive: [ // 반응형 웹 구현 옵션
+    draggable: true, 
+    responsive: [ 
       {
-        breakpoint: 960, //화면 사이즈 960px
+        breakpoint: 960, 
         settings: {
           slidesToShow: 4
         }
           },
       {
-        breakpoint: 768, //화면 사이즈 768px
+        breakpoint: 768, 
         settings: {
           slidesToShow: 5
         }
@@ -126,12 +126,7 @@ $(document).ready(function () {
     slidesPerView: 3,
     centeredSlides: true,
     loop: true,
-    spaceBetween: 0,
-    autoplay: { //자동슬라이드 (false-비활성화)         
-      delay: 10000, // 시간 설정          
-      disableOnInteraction: false,
-      // false-스와이프 후 자동 재생        
-    },
+    spaceBetween: -180,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
